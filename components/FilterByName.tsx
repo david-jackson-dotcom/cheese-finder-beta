3484a70be3de9776fded2663e16c1121cde1cde1
david-jackson-dotcom <getjackson@gmail.com>
2025-11-21 15:13:09 -0800

    Update FilterByName.tsx
    fix scrolling headers? orig in comments

--- components/FilterByName.tsx
+++ components/FilterByName.tsx
@@ -65,8 +65,11 @@ export function FilterByName({ onBack }: FilterByNameProps) {
   return (
     <div className="min-h-screen bg-background">
       {/* Header */}
-      <div className="top-0 z-10 bg-background">
-        <div className="relative px-6 py-14">
+     // <div className="top-0 z-10 bg-background">
+      //  <div className="relative px-6 py-14">
+         <div className="bg-gold">
+        <div className="px-6 pb-4">
+                <div className="relative px-6 py-14">
           <div className="absolute left-6 top-1/2 -translate-y-1/2">
             <Button
               onClick={onBack}
