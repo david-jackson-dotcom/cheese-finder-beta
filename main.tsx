c270605c8154ac60f3aca1a851022e2ef39bc43e
david-jackson-dotcom <getjackson@gmail.com>
2025-11-21 21:20:07 -0800

    Update main.tsx
    service worker relative url

--- main.tsx
+++ main.tsx
@@ -90,7 +90,7 @@ try {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('/service-worker.js')
+        .register('./service-worker.js')
         .then((registration) => {
           console.log('Service Worker registered successfully:', registration.scope);
           
