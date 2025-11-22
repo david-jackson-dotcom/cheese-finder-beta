09ed0d727a0c947684ae3e206392c25a25206381
david-jackson-dotcom <getjackson@gmail.com>
2025-11-21 21:49:49 -0800

    Update vite.config.ts
    line 7 was 
    
      base: '/cheese-finder-beta/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
-  base: '/cheese-finder-beta/
+  base: process.env.NODE_ENV === 'production' ? '/cheese-finder-beta/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
