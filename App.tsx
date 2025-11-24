73e41673436df6842088ca9e0336a170437678e4
david-jackson-dotcom <getjackson@gmail.com>
2025-11-23 17:46:55 -0800

    Update App.tsx
    line 805

--- App.tsx
+++ App.tsx
@@ -802,7 +802,7 @@ export default function App() {
       
       {screen === 'usage-selection' && (
         <WhatAreYouMaking 
-          onSelectUsage={handleSelectUsage} 
+          onSelectUsage={handleSelectUse} 
           onBack={() => setScreen('animal')} 
           onSubstituteSearch={handleSubstituteSearch}
           onSubstituteError={handleSubstituteError}
