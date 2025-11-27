a2fbf99129e475fa0315471d61a158adfc96887b
Apewalk <getjackson@gmail.com>
2025-11-27 14:56:43 -0800

    Update main.tsx
    corrected register address line 95

--- main.tsx
+++ main.tsx
@@ -92,7 +92,7 @@ try {
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register(`/service-worker.js`)
+      .register(`/cheese-finder-beta/service-worker.js`)
       .then((registration) => {
         console.log('Service Worker registered successfully:', registration.scope);
           
