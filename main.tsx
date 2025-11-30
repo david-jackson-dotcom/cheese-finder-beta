4fd1b78f8a110a6685628594bf3c24a3722f9faf
david-jackson-dotcom <getjackson@gmail.com>
2025-11-30 09:44:28 -0800

    sub url in sw

--- main.tsx
+++ main.tsx
@@ -74,7 +74,7 @@ createRoot(document.getElementById('root')!).render(
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
         navigator.serviceWorker
-          .register(`/service-worker.js`)
+          .register('/cheese-finder-beta/service-worker.js')
           .then((registration) => {
             console.log('Service Worker registered successfully:', registration.scope);  
             
