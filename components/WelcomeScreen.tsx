6ad83fb14455e2394a6e1400004ca9de78e15089
david-jackson-dotcom <getjackson@gmail.com>
2025-11-30 13:03:26 -0800

    don't import svg

--- components/WelcomeScreen.tsx
+++ components/WelcomeScreen.tsx
@@ -9,7 +9,6 @@ import {
   DialogTitle,
 } from './ui/dialog';
 import { toast } from 'sonner';
-import cheeseIcon from '../cheese-icon.svg';
 
 interface WelcomeScreenProps {
   onSelectMode: (mode: 'name' | 'taste' | 'region' | 'animal') => void;
